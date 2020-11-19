e93b033024c7e2846bae7adeaacfa819b8fa43eb
Michael Griffin <mike@munchdevelopment.com>
2020-11-19 12:35:52 -0600

    update build process

---
+++ cluey-build-test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# release channel test-v1-3
+
+expo build:ios --release-channel test-v1-3  --type archive --no-publish --config ./app.test.json
---
+++ cluey-upload-test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# release channel test-v1-3
+
+expo upload:ios --config ./app.test.json
